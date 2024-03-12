234cfdfb026a7ecd3b893c57fceda44674b9d7c6
mumuksh10 <mumuksh7@gmail.com>
2024-03-12 14:48:35 +0530

    Changed mail text

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -21,8 +21,21 @@ module.exports = async (email, subject, text) => {
 		await transporter.sendMail({
 			from: 'apoorv.info@gmail.com',
 			to: email,
-			subject: "SEMAMART EMAIL VERIFICATION",
-			text: "Please Enter This OTP on SEMAMART " + text,
+			subject: "Account Verification - SEMAMART",
+			text: 
+			`Dear User,
+
+			Thank you for choosing SEMMART! We're thrilled to have you on board. To ensure the security of your account, we require a quick verification step.
+			
+			Please use the following One-Time Passcode (OTP) to complete your registration:
+			
+			OTP: ${text}
+			
+			Welcome to SEMAMART!
+			
+			Best regards,
+			SEMAMART Team
+			`,
 		
 		});
 		console.log("email sent successfully");
